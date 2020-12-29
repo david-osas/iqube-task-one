000639d8fe69bc6c7f0bed29bc7384a08fc1dd54
david-osas <azamegbeosarumense@gmail.com>
2020-12-29 16:42:04 +0100

    Add routing

--- task-one/src/components/App.js
+++ task-one/src/components/App.js
@@ -1,11 +1,25 @@
+import {Switch, Route} from 'react-router-dom';
 import Form from './Form';
 
 function App() {
   return (
     <div className='app-container'>
-      <h1 className='app-heading'>Complete your Purchase</h1>
+      <Switch>
+        <Route exact path='/'>
+          <h1 className='app-heading'>Complete your Purchase</h1>
+          <Form />
+        </Route>
+        <Route exact path='/billing'>
+          <h1 className='app-heading'>Complete your Purchase</h1>
+          <Form />
+        </Route>
+        <Route exact path='/payment'>
+          <h1 className='app-heading'>Complete your Purchase</h1>
+          <Form />
+        </Route>
 
-      <Form />
+      </Switch>
+
     </div>
   );
 }
