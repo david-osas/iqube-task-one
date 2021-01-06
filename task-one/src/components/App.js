6ddc80ec950e84865a2d8ab6a2f3efaa3c41cf90
david-osas <azamegbeosarumense@gmail.com>
2021-01-06 15:59:46 +0100

    Rename css file and add styling for tabs

--- task-one/src/components/App.js
+++ task-one/src/components/App.js
@@ -6,28 +6,29 @@ import CompletePayment from './CompletePayment';
 import ButtonGroup from './ButtonGroup';
 
 function App() {
+  let stages = ['personal', 'billing', 'payment'];
   return (
     <div className='app-container'>
       <Switch>
         <Route exact path='/'>
           <h1 className='app-heading'>Complete your Purchase</h1>
-          <Tab />
-          <Form stage='personal' />
-          <ButtonGroup stage='personal' />
+          <Tab stage={stages[0]} />
+          <Form stage={stages[0]} />
+          <ButtonGroup stage={stages[0]} />
         </Route>
 
         <Route exact path='/billing'>
           <h1 className='app-heading'>Complete your Purchase</h1>
-          <Tab />
-          <Form stage='billing' />
-          <ButtonGroup stage='billing'/>
+          <Tab stage={stages[1]} />
+          <Form stage={stages[1]} />
+          <ButtonGroup stage={stages[1]}/>
         </Route>
 
         <Route exact path='/payment'>
           <h1 className='app-heading'>Complete your Purchase</h1>
-          <Tab />
+          <Tab stage={stages[2]} />
           <ConfirmPayment />
-          <ButtonGroup stage='payment'/>
+          <ButtonGroup stage={stages[2]}/>
         </Route>
 
         <Route exact path='/complete'>
--- task-one/src/components/Tab.js
+++ task-one/src/components/Tab.js
@@ -1,11 +1,35 @@
+import tabBar from '../bar.svg';
 
-function Tab(){
+function Tab({stage}){
+  let tabsColor = {personal: '', billing: '', payment: ''};
+  tabsColor[stage] = 'tab-color';
+  let barPosition = '';
+  switch(stage){
+    case 'personal':
+    barPosition = 'tab-bar-start';
+    break;
+
+    case 'billing':
+    barPosition = 'tab-bar-center';
+    break;
+
+    case 'payment':
+    barPosition = 'tab-bar-end';
+    break;
+
+    default:
+    barPosition = 'tab-bar-start';
+  }
 
   return(
-    <div className='app-tab'>
-      <span to='/' className='tab-link' >Personal Info</span>
-      <span to='/billing' className='tab-link' >Billing Info</span>
-      <span to='/payment' className='tab-link' >Confirm Payment</span>
+    <div>
+      <div className='app-tab'>
+        <span className={`tab-link ${tabsColor.personal}`} >Personal Info</span>
+        <span className={`tab-link ${tabsColor.billing}`} >Billing Info</span>
+        <span className={`tab-link ${tabsColor.payment}`} >Confirm Payment</span>
+      </div>
+      <img src={tabBar} alt='tab bar' className={`tab-bar ${barPosition}`} />
+      <hr />
     </div>
   );
 }
